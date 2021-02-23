43b31cd4c7842f3e44e8a2032cf8087e9c95cac8
Gonçalo Ventura Cruz <79382642+Venturinha27@users.noreply.github.com>
2021-02-23 01:37:49 +0000

    atualizacaoV3

--- Base de Dados/BaseDeDados.sql
+++ Base de Dados/BaseDeDados.sql
@@ -3,29 +3,29 @@ CREATE TABLE RegistoVoluntarios (
     dataNascimento date, 
     genero varchar(255),         --tipo errado
     foto varbinary(max),           
-    concelho varchar(50)
-    distrito varchar(50)
-    freguesia varchar(50)
-    telefone numeric(15)
-    CC numeric(8)
-    CartaC varchar(255)         --tipo errado
-    Covid varchar(255)          --tipo errado
-    E-mail nvarchar(255)
-    password1 varchar(20)
+    concelho varchar(50),
+    distrito varchar(50),
+    freguesia varchar(50),
+    telefone numeric(15),
+    CC numeric(8) UNIQUE,
+    CartaC varchar(255),        --tipo errado
+    Covid varchar(255),         --tipo errado
+    E-mail nvarchar(255),
+    password1 varchar(20),
 );
 
 
 CREATE TABLE RegistoInstituicoes (
     nomeInstituicao varchar(15),
     descricao varchar(200),
-    telefone numeric(15)
-    morada varchar(100)
-    distrito varchar(50)
-    concelho varchar(50)
-    freguesia varchar(50)
-    E-mail nvarchar(255)
-    website varchar(50)
+    telefone numeric(15),
+    morada varchar(100),
+    distrito varchar(50),
+    concelho varchar(50),
+    freguesia varchar(50),
+    E-mail nvarchar(255),
+    website varchar(50),
     nomeRepresentante varchar(15),
-    emailRepresentante nvarchar(255)
-    password2 varchar(20)
+    emailRepresentante nvarchar(255),
+    password2 varchar(20),
 );
