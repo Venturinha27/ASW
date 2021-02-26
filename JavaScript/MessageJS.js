aec16693e6a396ff2e8c2c70e19c1c3e1f6000b7
Tiago Teodoro <tiagoteo2001@gmail.com>
2021-02-26 12:57:55 +0000

    Update MessageJs

--- JavaScript/MessageJS.js
+++ JavaScript/MessageJS.js
@@ -1,6 +1,23 @@
 window.addEventListener("load", principal);
 
 function principal() {
+    let men = document.getElementById("openMensagens")
+    let mdiv = document.getElementById("MessageDiv")
+    let head = document.getElementById("AzulDiv")
+    let bod = document.getElementById("BodyDiv")
+    if (sessionStorage.getItem("MessageBar") != "Closed"){
+        mdiv.setAttribute("class", "w3-sidebar visible")
+        men.setAttribute("class", "divOpen")
+        head.style.left = "0%"
+        bod.style.left = "0%"
+        sessionStorage.setItem("MessageBar", "Open")
+    } else {
+        mdiv.setAttribute("class", "w3-sidebar hidden")
+        men.setAttribute("class", "divClosed")
+        head.style.left = "15%"
+        bod.style.left = "15%"
+        sessionStorage.setItem("MessageBar", "Closed")
+    }
     clickM();
 }
 
@@ -10,16 +27,19 @@ function clickM() {
     let head = document.getElementById("AzulDiv")
     let bod = document.getElementById("BodyDiv")
     men.addEventListener("click", function() {
-        if (mdiv.getAttribute("class") == "w3-sidebar hidden"){
+        //if (mdiv.getAttribute("class") == "w3-sidebar hidden"){
+        if (sessionStorage.getItem("MessageBar") != "Open"){
             mdiv.setAttribute("class", "w3-sidebar visible")
             men.setAttribute("class", "divOpen")
             head.style.left = "0%"
             bod.style.left = "0%"
+            sessionStorage.setItem("MessageBar", "Open")
         } else {
             mdiv.setAttribute("class", "w3-sidebar hidden")
             men.setAttribute("class", "divClosed")
             head.style.left = "15%"
             bod.style.left = "15%"
+            sessionStorage.setItem("MessageBar", "Closed")
         }
     });
 }
