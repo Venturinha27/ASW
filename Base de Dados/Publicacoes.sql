7456e25131c306a239291c5f339680534eed9cc2
Gonçalo Ventura Cruz <79382642+Venturinha27@users.noreply.github.com>
2021-02-26 18:23:58 +0000

    sql

--- Base de Dados/Publicacoes.sql
+++ Base de Dados/Publicacoes.sql
@@ -1,6 +1,26 @@
 CREATE TABLE  Publicacoes{
-    dono VARCHAR (50);
-    imagem varbinary(max);
-    participantes VARCHAR (50);
-    descricao VARCHAR (150);
+    id NUMERIC (10000) ,
+    dono VARCHAR (50),
+    imagem varbinary(max),
+    participantes VARCHAR (50),
+    descricao VARCHAR (150),
+
+    CONSTRAINT pk_publicacoes 
+        PRIMARY KEY (id),
+        
+    CONSTRAINT fk_dono_publicacoes 
+        FOREIGN KEY (dono) REFERENCES (Publicacoes)
 }
+
+CREATE TABLE  Participa_em_publicacao{
+    id NUMERIC (10000);
+    participante VARCHAR (50);
+    
+}
+
+CREATE TABLE  Publicacoes{
+    id NUMERIC (10000) ,
+    dono VARCHAR (50),
+    imagem varbinary(max),
+    participantes VARCHAR (50),
+    descricao VARCHAR (150),
---
+++ Base de Dados/utilizador.sql
@@ -0,0 +1,4 @@
+CREATE TABLE  utilizador{
+    id NUMERIC (10000) ,
+    tipo VARCHAR (50) ,
+    
