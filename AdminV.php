ccc811b07eb9c4319f844ff3ac14f19e7ebfeded
Tiago Teodoro <tiagoteo2001@gmail.com>
2021-03-12 14:15:33 +0000

    update adminv

--- AdminV.php
+++ AdminV.php
@@ -57,8 +57,6 @@
                 <div id="esq">
                     <input type="text" class="w3-input" name="nome" placeholder="Nome" name="nome"/>
                     <br>
-                    <input type="text" class="w3-input" name="email" placeholder="Email" name="email"/>
-                    <br>
                     <label><b>Idade</b></label>
                     <select class="w3-input" name="idade">
                         <option value="" disabled selected>Idade</option>
@@ -104,8 +102,7 @@
                         <option value="2">2</option>
                         <option value="3">3</option>
                     </select>
-                </div>
-                <div id="dir">
+                    <br>
                     <label><b>Freguesia</b></label>
                     <select class="w3-input" name="freguesia">
                         <option value="" disabled selected>Freguesia</option>
@@ -121,6 +118,9 @@
                         <option value="Mulher">Mulher</option>
                         <option value="Prefiro não dizer">Prefiro não dizer</option>
                     </select>
+                </div>
+                <div id="dir">
+                    <input type="text" class="w3-input" name="email" placeholder="Email" name="email"/>
                     <br>
                     <label><b>Carta de Condução</b></label>
                     <select class="w3-input" name="carta">
@@ -135,18 +135,253 @@
                         <option value="Sim">Sim</option>
                         <option value="Não">Não</option>
                     </select>
+                    <br>
+                    <label><b>Áreas de interesse:</b></label>
+                    <select class="w3-input" name="area-interesse">
+                        <option value="" disabled selected>Área de interesse</option>
+                        <option value="Ação social">Ação social</option>
+                        <option value="Educação">Educação</option>
+                        <option value="Saúde">Saúde</option>
+                    </select>
+                    <br>
+                    <label><b>População-alvo:</b></label>
+                    <select class="w3-input" name="populacao-alvo">
+                        <option value="" disabled selected>Selecione a sua população-alvo</option>
+                        <option value="Indiferente">Indiferente</option>
+                        <option value="Crianças">Crianças</option>
+                        <option value="Jovens">Jovens</option>
+                        <option value="Idosos">Idosos</option>
+                        <option value="Grávidas">Grávidas</option>
+                        <option value="Pessoas em situação de dependência (ex. acamados)">Pessoas em situação de dependência (ex. acamados)</option>
+                        <option value="Pessoas sem-abrigo">Pessoas sem-abrigo</option>
+                        <option value="Pessoas com deficiência">Pessoas com deficiência</option>
+                    </select>
+                    <br>
+                    <label><b>Disponibilidade:</b></label>
+                    <br>
+                    <select class="w3-input dis" name="disponibilidade-dia">
+                        <option value="" disabled selected>Dia</option>
+                        <option value="Segunda-feira">Segunda-feira</option>
+                        <option value="Terça-feira">Terça-feira</option>
+                        <option value="Quarta-feira">Quarta-feira</option>
+                        <option value="Quinta-feira">Quinta-feira</option>
+                        <option value="Sexta-feira">Sexta-feira</option>
+                        <option value="Sábado">Sábado</option>
+                        <option value="Domingo">Domingo</option>
+                    </select>
+                    <select class="w3-input dis" name="disponibilidade-hora">
+                        <option value="" disabled selected>Hora</option>
+                        <option value="00:00">00:00</option>
+                        <option value="01:00">01:00</option>
+                        <option value="02:00">02:00</option>
+                        <option value="03:00">03:00</option>
+                        <option value="04:00">04:00</option>
+                        <option value="05:00">05:00</option>
+                        <option value="06:00">06:00</option>
+                        <option value="07:00">07:00</option>
+                        <option value="08:00">08:00</option>
+                        <option value="09:00">09:00</option>
+                        <option value="10:00">10:00</option>
+                        <option value="11:00">11:00</option>
+                        <option value="12:00">12:00</option>
+                        <option value="13:00">13:00</option>
+                        <option value="14:00">14:00</option>
+                        <option value="15:00">15:00</option>
+                        <option value="16:00">16:00</option>
+                        <option value="17:00">17:00</option>
+                        <option value="18:00">18:00</option>
+                        <option value="19:00">19:00</option>
+                        <option value="20:00">20:00</option>
+                        <option value="21:00">21:00</option>
+                        <option value="22:00">22:00</option>
+                        <option value="23:00">23:00</option>
+                    </select>
+                    <select class="w3-input dis" name="disponibilidade-duracao">
+                        <option value="" disabled selected>Duração</option>
+                        <option value="1">01:00</option>
+                        <option value="2">02:00</option>
+                        <option value="3">03:00</option>
+                        <option value="4">04:00</option>
+                        <option value="5">05:00</option>
+                        <option value="6">06:00</option>
+                        <option value="7">07:00</option>
+                        <option value="8">08:00</option>
+                    </select>
+                    
                 </div>
                 <input class="w3-button" id="procura" type="submit" value="Procura"/>
             </form>
 </div>
 
 <?php
-    include "openconn.php";
+    include "openconn.php"; 
 
-    $queryVoluntario = "SELECT id, nome_voluntario, bio, data_nascimento, genero, concelho
-                        , distrito, freguesia, telefone, cc, carta_c, covid, email
-                        FROM Voluntario
-                        ORDER BY nome_voluntario;";
+    if (!empty($_POST)){
+
+        $primeiro = 0;
+
+        $queryVoluntario = "SELECT id, nome_voluntario, bio, data_nascimento, genero, concelho
+                            , distrito, freguesia, telefone, cc, carta_c, covid, email
+                            FROM Voluntario ";
+
+        if (!empty($_POST['nome'])){
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE nome_voluntario = '".$_POST['nome']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND nome_voluntario = '".$_POST['nome']."' ";
+            }
+        }
+
+        if (!empty($_POST['email'])) {
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE email = '".$_POST['email']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND email = '".$_POST['email']."' ";
+            }
+        }
+
+        if (!empty($_POST['idade'])) {
+            
+            if ($_POST['idade'] == "10 aos 20") {
+                $time1 = strtotime("-10 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-20 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($_POST['idade'] == "21 aos 30") {
+                $time1 = strtotime("-20 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-30 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($_POST['idade'] == "31 aos 40") {
+                $time1 = strtotime("-30 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-40 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($_POST['idade'] == "41 aos 50") {
+                $time1 = strtotime("-40 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-50 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($_POST['idade'] == "51 aos 60") {
+                $time1 = strtotime("-50 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-60 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($_POST['idade'] == "61 aos 70") {
+                $time1 = strtotime("-60 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-70 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($_POST['idade'] == "71 aos 80") {
+                $time1 = strtotime("-70 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-80 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($_POST['idade'] == "81+") {
+                $time1 = strtotime("-80 years", time());
+                $date1 = date("Y-m-d", $time1);
+                $time2 = strtotime("-150 years", time());
+                $date2 = date("Y-m-d", $time2);
+            }
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE (data_nascimento BETWEEN '".$date2."' AND '".$date1."') ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND (data_nascimento BETWEEN '".$date2."' AND '".$date1."') ";
+            }
+        }
+
+        if (!empty($_POST['distrito'])) {
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE distrito = '".$_POST['distrito']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND distrito = '".$_POST['distrito']."' ";
+            }
+        }
+
+        if (!empty($_POST['concelho'])) {
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE concelho = '".$_POST['concelho']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND concelho = '".$_POST['concelho']."' ";
+            }
+        }
+
+        if (!empty($_POST['freguesia'])) {
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE freguesia = '".$_POST['freguesia']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND freguesia = '".$_POST['freguesia']."' ";
+            }
+        }
+
+        if (!empty($_POST['genero'])) {
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE genero = '".$_POST['genero']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND genero = '".$_POST['genero']."' ";
+            }
+        }
+
+        if (!empty($_POST['carta'])) {
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE carta_c = '".$_POST['carta']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND carta_c = '".$_POST['carta']."' ";
+            }
+        }
+
+        if (!empty($_POST['covid'])) {
+            if ($primeiro == 0){
+                $queryVoluntario .= "WHERE covid = '".$_POST['covid']."' ";
+                $primeiro = 1;
+            } else {
+                $queryVoluntario .= "AND covid = '".$_POST['covid']."' ";
+            }
+        }
+
+        if (!empty($_POST['area-interesse'])) {
+            
+            $queryVoluntario .= "WHERE id IN (SELECT id_voluntario
+                                FROM Voluntario_Area
+                                WHERE area = '".$_POST['area-interesse']."') ";
+            
+            #echo "<h1 class='entrou'>".$queryVoluntario."</h1>";
+        }
+
+        $queryVoluntario .= "ORDER BY nome_voluntario ";
+        
+
+        #echo "<h1 class='entrou'>".$queryVoluntario."</h1>";
+        
+        #echo "<h1 class='entrou'>".$today."</h1>";
+        #echo "<h1 class='entrou'>".$_POST['idade']."</h1>";
+        #echo "<h1 class='entrou'>".$_POST['distrito']."</h1>";
+        #echo "<h1 class='entrou'>".$_POST['concelho']."</h1>";
+        #echo "<h1 class='entrou'>".$_POST['freguesia']."</h1>";
+        #echo "<h1 class='entrou'>".$_POST['genero']."</h1>";
+        #echo "<h1 class='entrou'>".$_POST['carta']."</h1>";
+        #echo "<h1 class='entrou'>".$_POST['covid']."</h1>";
+    } else {
+        $queryVoluntario = "SELECT id, nome_voluntario, bio, data_nascimento, genero, concelho
+                    , distrito, freguesia, telefone, cc, carta_c, covid, email
+                    FROM Voluntario
+                    ORDER BY nome_voluntario;";
+    }
 
     $resultVoluntario = $conn->query($queryVoluntario);
 
@@ -154,7 +389,7 @@
         echo "Erro: search failed" . mysqli_error($conn);
     }              
 
-    echo "<table class='w3-table w3-striped w3-small w3-hoverable' id='todosVol'>
+    echo "<table class='w3-table w3-striped w3-tiny w3-hoverable w3-middle' id='todosVol'>
             <tr class='w3-red'>
                 <th>Nome</th>
                 <th>Bio</th>
@@ -168,29 +403,75 @@
                 <th>Carta C.</th>
                 <th>Covid</th>
                 <th>Email</th>
+                <th>Áreas Interesse</th>
+                <th>População Alvo</th>
+                <th>Disponibilidade</th>
             </tr>";
     if ($resultVoluntario->num_rows > 0) {
         
         while ($row = $resultVoluntario->fetch_assoc()){
             echo "
             <tr>
-                <td>".$row['nome_voluntario']."</td>
-                <td>".$row['bio']."</td>
-                <td>".$row['data_nascimento']."</td>
-                <td>".$row['genero']."</td>
-                <td>".$row['distrito']."</td>
-                <td>".$row['concelho']."</td>
-                <td>".$row['freguesia']."</td>
-                <td>".$row['telefone']."</td>
-                <td>".$row['cc']."</td>
-                <td>".$row['carta_c']."</td>
-                <td>".$row['covid']."</td>
-                <td>".$row['email']."</td>
-            </tr>
+                <td><p>".$row['nome_voluntario']."</p></td>
+                <td><p>".$row['bio']."</p></td>
+                <td><p>".$row['data_nascimento']."</p></td>
+                <td><p>".$row['genero']."</p></td>
+                <td><p>".$row['distrito']."</p></td>
+                <td><p>".$row['concelho']."</p></td>
+                <td><p>".$row['freguesia']."</p></td>
+                <td><p>".$row['telefone']."</p></td>
+                <td><p>".$row['cc']."</p></td>
+                <td><p>".$row['carta_c']."</p></td>
+                <td><p>".$row['covid']."</p></td>
+                <td><p>".$row['email']."</p></td>";
+
+                $queryArea = "SELECT id_voluntario, area
+                    FROM Voluntario_Area
+                    WHERE id_voluntario = '".$row['id']."'";
+
+                $resultArea = $conn->query($queryArea);            
+
+                echo "<td>";
+                if ($resultArea->num_rows > 0) {
+                    while ($rowA = $resultArea->fetch_assoc()){
+                        echo "<p>" . $rowA['area'] . "</p>";
+                    }
+                }
+                echo "</td>";
+
+                $queryPopulacao = "SELECT id_voluntario, populacao_alvo
+                    FROM Voluntario_Populacao_Alvo
+                    WHERE id_voluntario = '".$row['id']."'";
+
+                $resultPopulacao = $conn->query($queryPopulacao);            
+
+                echo "<td>";
+                if ($resultPopulacao->num_rows > 0) {
+                    while ($rowP = $resultPopulacao->fetch_assoc()){
+                        echo "<p>" . $rowP['populacao_alvo'] . "</p>";
+                    }
+                }
+                echo "</td>";
+
+                $queryDispo = "SELECT id_voluntario, dia, hora, duracao
+                    FROM Voluntario_Disponibilidade
+                    WHERE id_voluntario = '".$row['id']."'";
+
+                $resultDispo = $conn->query($queryDispo);            
+
+                echo "<td>";
+                if ($resultDispo->num_rows > 0) {
+                    while ($rowD = $resultDispo->fetch_assoc()){
+                        echo "<p>" . $rowD['dia'] . ", às ". $rowD['hora'] .", durante ". $rowD['duracao'] ." horas.</p>";
+                    }
+                }
+                echo "</td>";
+
+            echo "</tr>
             ";
         }
     }
-    echo "</table><br><br><br>";
+    echo "</table>";
 ?>
 
 </body>
