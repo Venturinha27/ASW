e9ff46b257b6123ce8b7f4d0ed23723afbfb6597
Tiago Teodoro <tiagoteo2001@gmail.com>
2021-03-16 11:24:21 +0000

    html para php, logout em todas as paginas

--- PreferenciasI.php
+++ PreferenciasI.php
@@ -22,16 +22,76 @@
 
 <header>
     <div class="w3-bar w3-large" id="navigation">
-        <a href="HomePage.html" class="w3-bar-item w3-button w3-hover-blue w3-mobile">VoluntárioCOVID19</a>
+        <a href="HomePage.php" class="w3-bar-item w3-button w3-hover-blue w3-mobile">VoluntárioCOVID19</a>
 
         <input type="text" class="w3-bar-item w3-input" placeholder="Procura...">
         
-        <a href="Login.html" class="w3-bar-item w3-button w3-blue w3-hover-blue w3-right w3-mobile"><i class="fa fa-user-circle"></i></a>
-        <a href="Voluntarios.html" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">Voluntários</a>
-        <a href="Instituicoes.html" class="w3-bar-item w3-button  w3-hover-blue w3-right w3-mobile">Instituições</a>
-        <a href="Covid19.html" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">COVID-19</a>
-        <a href="Publicacoes.html" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">Publicações</a>   
-        <a href="Sobre.html" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">Sobre</a>    
+        <?php
+            include 'openconn.php';
+
+            if (!isset($_SESSION['logged'])) {
+                echo "<a href='Perfil.php' class='w3-bar-item w3-button w3-hover-blue w3-right w3-mobile'><i class='fa fa-user-circle'></i></a>";
+            } else {
+                $queryUtilizador = "SELECT id, tipo 
+                            FROM Utilizador 
+                            WHERE id = '".$_SESSION['loggedid']."';";
+
+                $resultUtilizador = $conn->query($queryUtilizador);
+
+                if ($row = $resultUtilizador->fetch_assoc()){
+                    
+                    if ($row['tipo'] == "voluntario"){
+                        $queryVoluntario = "SELECT id, foto
+                            FROM Voluntario
+                            WHERE id = '".$_SESSION['loggedid']."';";
+
+                        $resultVoluntario = $conn->query($queryVoluntario);
+
+                        if ($rowV = $resultVoluntario->fetch_assoc()){
+                            $foto = $rowV['foto'];
+                        }
+                    } else {
+                        $queryInstituicao = "SELECT id, foto
+                            FROM Instituicao
+                            WHERE id = '".$_SESSION['loggedid']."';";
+
+                        $resultInstituicao = $conn->query($queryInstituicao);
+
+                        if ($rowI = $resultInstituicao->fetch_assoc()){
+                            $foto = $rowI['foto'];
+                        }
+                    }
+                }
+
+                echo "<div class='w3-dropdown-hover w3-right w3-mobile'>
+                        <button class='w3-button w3-hover-blue'>
+                            <img alt='Avatar' class='w3-circle' id='foto' src='$foto' style='width:26px; height: 26px;'/>
+                        </button>
+                        <div class='w3-dropdown-content w3-bar-block w3-card-4 w3-left w3-small' style='right:0%; z-index: 100; width:10%;'>
+                            <a href='Perfil.php' class='w3-bar-item w3-button'>Ver perfil</a>
+                            <a href='EditarPerfil.php' class='w3-bar-item w3-button'>Editar perfil</a>
+                            <form action='".htmlspecialchars($_SERVER['PHP_SELF'])."' method='post'>
+                                <button type='submit' value='terminarS' name='terminarS' class='w3-bar-item w3-button w3-white w3-text-red'>Terminar sessão</button>
+                            </form>
+                        </div>
+                    </div>";
+            }
+
+            if ($_POST['terminarS']){
+                unset ($_SESSION['loggedtype']);
+                unset ($_SESSION['logged']);
+                unset ($_SESSION['loggedid']);
+                unset ($_SESSION['opentype']);
+                unset ($_SESSION['open']);
+                unset ($_SESSION['openid']);
+                echo "<meta http-equiv='refresh' content='0'>";
+            }
+        ?>
+        <a href="Voluntarios.php" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">Voluntários</a>
+        <a href="Instituicoes.php" class="w3-bar-item w3-button  w3-hover-blue w3-right w3-mobile">Instituições</a>
+        <a href="Covid19.php" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">COVID-19</a>
+        <a href="Publicacoes.php" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">Publicações</a>   
+        <a href="Sobre.php" class="w3-bar-item w3-button w3-hover-blue w3-right w3-mobile">Sobre</a>    
     </div>
 </header>
 
@@ -348,16 +408,16 @@
         <div class="vl"></div>
 
         <ul id="endPaginas1">
-            <a href="Sobre.html"><li>Sobre</li></a>
+            <a href="Sobre.php"><li>Sobre</li></a>
             <br>
-            <a href="#"><li>Publicações</li></a>
+            <a href="Publicacoes.php"><li>Publicações</li></a>
             <br>
-            <a href="Covid19.html"><li>COVID-19</li></a>
+            <a href="Covid19.php"><li>COVID-19</li></a>
         </ul>
         <ul id="endPaginas2">
-            <a href="Instituicoes.html"><li>Instituições</li></a>
+            <a href="Instituicoes.php"><li>Instituições</li></a>
             <br>
-            <a href="Voluntarios.html"><li>Voluntários</li></a>
+            <a href="Voluntarios.php"><li>Voluntários</li></a>
         </ul>
 
         <p id="endD">Todos os direitos reservados a Gonçalo Ventura, Margarida Rodrigues, Renato Ramires e Tiago Teodoro</p>
