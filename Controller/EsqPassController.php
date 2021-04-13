e54e009609516c16df7e52da23641fd16c7c7a98
Maggie <margaridarodrigues4a@gmail.com>
2021-04-13 22:52:44 +0100

    esqPass

--- Controller/EsqPassController.php
+++ Controller/EsqPassController.php
@@ -2,8 +2,31 @@
 
     include "../Model/Model.php";
 
-    function esqPass(){
-        
+    function esqPass($email, $telefone, $novaPassword, $confPassword){
+
+        $resposta = esqpass_query();
+
+        while ($row = $resposta->fetch_array()){
+            if ($email == $row[0] and $telefone == $row[1]){
+                if ($novaPassword == $confPassword) {
+                    if ($row[3] == 'voluntario'){
+                        $query = updateV();
+                    } else {
+                        $query = updateI();
+                    }
+                    
+                    $novaPass = $conn->query($query);
+
+                    if ($novaPass) {
+                        header("Location: Login.php");
+                    }
+                } else {
+                    echo "<p class='erro'> Passwords não coincidem <p>";
+                }
+            } else {
+                echo "<p class='erro'> Email e telefone não correspondem <p>";
+            }
+        }
     }
 
 ?>
